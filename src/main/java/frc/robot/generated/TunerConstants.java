520507e1d8384d049726fe1fb4e7c515ff56542a
Pumpkin Bots <pumpkinbots@hmbrobotics.org>
2025-02-16 11:42:17 -0800

    pathplanner changes

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,13 +24,13 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.5)// 100 , 0 , 0.5
+        .withKP(100).withKI(0).withKD(0.5)// original values: 100 , 0 , 0.5
         .withKS(0.1).withKV(1.5).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0) // 0.1, 0, 0
+        .withKP(0.1).withKI(0).withKD(0) // original values: 0.1, 0, 0
         .withKS(0).withKV(0.124);
 
     // The closed-loop output type to use for the steer motors;
