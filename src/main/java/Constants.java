4dbe031f04f6d6b73ef794af798f825f8349af5b
Pumpkin Bots <pumpkinbots@hmbrobotics.org>
2025-02-21 21:17:16 -0800

    added constants

---
+++ src/main/java/Constants.java
@@ -0,0 +1,7 @@
+public final class Constants {
+    public static class CoralLauncher {
+        public static final int kIntakeId = 17;
+        public static final int kOuttakeId = 16;
+
+    }
+}
