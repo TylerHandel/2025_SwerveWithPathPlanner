421c7e03c5c54f4ae8ed2d587319b5383a8bcdec
Alex <emrieaj@gmail.com>
2025-03-25 10:32:06 -0700

    Fix build error with file naming; minor formatting in Intake

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CoralLauncher;
 import frc.robot.subsystems.CoralSensor;
@@ -8,7 +7,6 @@ import frc.robot.subsystems.CoralSensor;
 public class Intake extends Command{
 
     private CoralLauncher m_launcher;
-    private Timer time = new Timer();
 
     public Intake(CoralLauncher launcher) {
         m_launcher = launcher;
@@ -20,9 +18,9 @@ public class Intake extends Command{
         m_launcher.setIntakeWheel(0.25);
         m_launcher.setOuttakeWheel(0);
     }
+
     @Override
     public boolean isFinished() {
-        
         return CoralSensor.isCoralDetected();
     }
 
