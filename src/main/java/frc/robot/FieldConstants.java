de69f054cfb5e14d3e1ddda8549bd442891e4eff
Alex <emrieaj@gmail.com>
2025-03-25 11:32:16 -0700

    Initialize FieldConstants

---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+/**
+ * Contains various field dimensions and useful reference points. All units are in meters and poses
+ * have a blue alliance origin.
+ */
+public class FieldConstants {
+
+}
